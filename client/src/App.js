8092e5ebd1e0e0752ae138aa24e97a14807eb109
santosh kumar dash <santoshkumardash205@gmail.com>
2023-11-25 13:37:33 +0530

    Routes defined

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Routes , Route} from 'react-router-dom';
+import Login from './Pages/auth/Login';
+import Register from './Pages/auth/Register';
+import Home from './Pages/Home/home';
 
-const App = () => (
-  <div>
-    <p>Hi this is the react app.</p>
-  </div>
-);
+const App = () => {
+  
+  return (
+    
+    <Routes>
+      <Route exact path="/" Component={Home}/>
+      <Route exact path="/login" Component={Login}/>
+      <Route exact path="/register" Component={Register}/>
+    </Routes>
+  );
+}
 
 export default App;
--- client/src/Pages/auth/Register.js
+++ client/src/Pages/auth/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Register = () => {
-    return(
+    return (
         <div>
             <p>Register</p>
         </div>
